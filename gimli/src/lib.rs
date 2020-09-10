3844b8c7856d49dcde70e1202186bf71bde6e0fe
Lzu Tao <taolzu@gmail.com>
2020-09-10 18:06:33 +0700

    mcve for 74672

--- gimli/src/lib.rs
+++ gimli/src/lib.rs
@@ -1,7 +1,8 @@
 #![feature(staged_api)]
+#![unstable(feature = "rustc_private", issue = "none")]
 
-#[unstable(feature = "rustc_private", issue = "none")]
+// #[unstable(feature = "rustc_private", issue = "none")]
 pub struct EndianSlice;
 
-#[unstable(feature = "rustc_private", issue = "none")]
+// #[unstable(feature = "rustc_private", issue = "none")]
 impl unstabled::Deref for EndianSlice {}
