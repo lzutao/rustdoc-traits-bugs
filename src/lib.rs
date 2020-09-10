a6e7f03b0984db62cf753964b8d4bf46477dafce
Lzu Tao <taolzu@gmail.com>
2020-09-10 18:06:33 +0700

    mcve for 74672

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(extremely_unstable_foo)]
 
+#[allow(unused)]
 extern crate gimli;
+extern crate unstabled;
 
-pub use unstabled::Join;
 pub use unstabled::Deref;
+pub use unstabled::Join;
